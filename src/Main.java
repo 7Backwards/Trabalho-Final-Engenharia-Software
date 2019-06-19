66d424213f7597dd37b5e2d3daf60552e0126a45
KiritoDC <49528570+KiritoDC@users.noreply.github.com>
2019-06-19 22:24:17 +0100

    Testes , 2ºsprint not working

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,23 @@
+import java.sql.SQLOutput;
 import java.time.LocalDate;
+import java.time.LocalTime;
 
 public class Main {
 
     public static void main(String[] args) {
 
 
+        Utilizador u = new Utilizador("Manuel",new TipoUtilizador("aluno",10),"Ativo");
+        PropostaAquisicao propostaAquisicao = new PropostaAquisicao(LocalDate.now(),u,"Alice no Pais das Maravilhas");
+        Livro l = new Livro("Programar em Java", "FCA");
+        Copia c = new Copia(1, l);
+        RequisicaoCompraCopia requisicaoCompraCopia = new RequisicaoCompraCopia("pendente", LocalDate.now(), "Oficio",l,u);
+        Encomenda encomenda = new Encomenda(LocalDate.now(),requisicaoCompraCopia);
+        EntradaNovoLivro entradaNovoLivro = new EntradaNovoLivro(LocalDate.now(),encomenda,c);
         RepositorioMem rep = new RepositorioMem();
+        rep.adicionaEntradaNovoLivro(entradaNovoLivro);
+
+
 
         Disciplina disciplina =new Disciplina("POO");
         Professor prof = new Professor(disciplina,"Jose");
@@ -18,8 +30,6 @@ public class Main {
 
         rep.adicionaProfessor(prof);rep.adicionaProfessor(prof2);rep.adicionaProfessor(prof3);
 
-        Funcionario funcionario = new Funcionario("Peter");
-
         SalaEstudo salaEstudo = new SalaEstudo(1,10,prof,rep.UtilizadorL);
 
         SalaEstudo salaEstudo2 = new SalaEstudo(2,10,prof2,rep.UtilizadorL);
@@ -29,18 +39,25 @@ public class Main {
 
 
 
-        //Testes do 3º sprint
-        rep.publicarHorarios(funcionario,salaEstudo);
+        //Testes do 3º sprint       Defenir horarios
 
-        System.out.println("___________________");
 
-        /*rep.publicarHorarios(funcionario,salaEstudo2);
 
-        System.out.println("____________________");
+        Funcionario funcionario = new Funcionario("Luis",LocalTime.of(8,00,00),LocalTime.of(17,00,00),LocalTime.of(13,00,00),LocalTime.of(14,00,00));
 
-        rep.publicarHorarios(funcionario,salaEstudo3);*/
+        rep.CriarHorarioProfessor(funcionario,prof,LocalTime.of(8,00,00),LocalTime.of(15,00,00),LocalTime.of(13,00,00),LocalTime.of(14,00,00));
 
-        //Testes ao 1º Sprint
+        System.out.println("Horario do funcionario");
+        System.out.println("Inicio: " + funcionario.getHorario().getHora_Inicio());
+        System.out.println("Fim: " + funcionario.getHorario().getHora_Fim());
+
+        System.out.println("_______");
+
+        System.out.println("Horario do Prof");
+        System.out.println("Inicio: "+ prof.getHorario().getHora_Inicio());
+        System.out.println("Fim: " + prof.getHorario().getHora_Fim());
+
+        //Testes ao 1º Sprint       Entrada e saida de alunos
 
         Utilizador aluno = new Utilizador("Kayle",null,null);
         Utilizador aluno2 = new Utilizador("Lucius",null,null);
@@ -51,8 +68,23 @@ public class Main {
         rep.entradaAlunoNaSala(salaEstudo,aluno2);
         rep.entradaAlunoNaSala(salaEstudo,aluno3);
         //rep.saidaAlunoNaSala(salaEstudo,aluno3);
-        rep.FecharSalaDeAula(salaEstudo);
+        //rep.FecharSalaDeAula(salaEstudo);
 
+
+        //Testes ao 2º Sprint       Entrega e avaliaçao dinamica da ficha
+        Fichas ficha = new Fichas("Geometria",disciplina3,12);
+
+        System.out.println("Dificuldade da ficha Inicio: " + ficha.getDificuldade());
+
+        rep.EntregarFichaAAluno(ficha,aluno);
+        rep.EntregarFichaAAluno(ficha,aluno2);
+        rep.EntregarFichaAAluno(ficha,aluno3);
+
+        rep.AlunoAvaliaFicha(aluno,ficha,14);
+        rep.AlunoAvaliaFicha(aluno2,ficha,16);
+        rep.AlunoAvaliaFicha(aluno3,ficha,16);
+
+        System.out.println("Dificuldade da ficha depois de avaliada pelos alunos: " + ficha.getDificuldade());
 
         System.out.println(salaEstudo.getAlunosList().size());
 
